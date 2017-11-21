Fix bugs in A-softmax loss implementation.

layers/margin_fully_connected.py
@@ -57,25 +57,23 @@ class Margin_Dense(base.Layer):
 
     def call(self, inputs, labels, global_step):
         inputs = tf.convert_to_tensor(inputs, dtype=self.dtype)
-        # norms
         inputs_norm = tf.norm(inputs, axis=1, keep_dims=True, name='inputs_norm')
-        kernel_norm = tf.norm(self.kernel, axis=0, keep_dims=True, name='kernel_norm')
-        # output inference
-        outputs = tf.matmul(inputs, self.kernel/kernel_norm)
-        theta = tf.acos(outputs/inputs_norm)
+        outputs = tf.matmul(inputs, tf.nn.l2_normalize(self.kernel, dim=0))
+        theta = tf.acos(outputs/(inputs_norm+1e-6))
         # shape
         output_shape = outputs.get_shape().as_list()
         # cos_theta
         nd_indices = tf.stack([tf.range(output_shape[0], dtype=tf.int32), labels], axis=1)
         label_outputs = tf.gather_nd(outputs, nd_indices)
         label_theta = tf.gather_nd(theta, nd_indices)
-        # beta
-        beta = tf.maximum(self.min_lambda, self.base*tf.pow(1+self.gamma*tf.cast(global_step, tf.float32), self.power))
+
+        beta = tf.maximum(5.0, 1500*tf.pow(1+0.1*tf.cast(global_step, tf.float32), -1))
+
         # sign
         k = tf.floor(self.m*label_theta/math.pi)
-        x_phi_theta = (tf.squeeze(inputs_norm)*(tf.cos(self.m*label_theta+k*math.pi)-2.*k) + beta*label_outputs)/(beta+1.0)
+        x_phi_theta = (tf.squeeze(inputs_norm)*(tf.cos(self.m*label_theta+k*math.pi)-2.*k) - label_outputs)/(1.0+beta)
         # m_theta update
-        m_outputs = tf.scatter_nd(nd_indices, x_phi_theta-label_outputs, shape=output_shape)
+        m_outputs = tf.scatter_nd(nd_indices, x_phi_theta, shape=output_shape)
         outputs += m_outputs
 
         if self.activation is not None: